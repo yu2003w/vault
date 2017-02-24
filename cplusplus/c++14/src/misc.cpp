768d69fdef5f1e5057e7e9fe5bfc587e7b05e284
yu2003w <yu2003w@hotmail.com>
2017-02-24 22:34:25 +0800

    Initially commits

---
+++ cplusplus/c++14/src/misc.cpp
@@ -0,0 +1,23 @@
+/*
+ * Initiated by Jared on Feb 23, 2017,
+ *
+ */
+
+#include "misc.hpp"
+
+MiscFeature::MiscFeature()
+{
+    data = 5;      
+}
+
+int MiscFeature::getData()
+{
+    return data;
+}
+
+bool MiscFeature::setData(int i)
+{
+    data = i;
+    return true;
+}
+
