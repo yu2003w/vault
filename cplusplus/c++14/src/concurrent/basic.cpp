37f2b8382f334a6c526ab2050c2800991a124b74
yu2003w <yu2003w@hotmail.com>
2017-03-02 15:37:56 +0800

    update source code

--- cplusplus/c++14/src/concurrent/basic.cpp
+++ cplusplus/c++14/src/concurrent/basic.cpp
@@ -9,6 +9,7 @@
 #include <thread>
 #include <chrono>
 #include <vector>
+#include <atomic>
 
 using namespace std;
 
@@ -33,6 +34,7 @@ void pause_thread(int n)
 void
 test_detach()
 {
+    cout<<"testing detach"<<endl;
     vector<thread> pool;
     for (int i=0; i < 5; i++) {
         pool.push_back(thread(pause_thread, 5*(i +1)));
@@ -40,12 +42,15 @@ test_detach()
 
     for (auto & th : pool)
         th.detach();
-    
+   
+    // here, if do not wait, detached threads could not write to stdout
+    pause_thread(30);   
 }
 
-int
-main(int argc, char** argv)
+void
+test_join()
 {
+    cout<<"testing join()"<<endl;
     thread first(fun);
     thread second(fun1, 5, "powerful thread support in c++11");
     
@@ -53,8 +58,47 @@ main(int argc, char** argv)
 
     first.join();
     second.join();
+}
+
+atomic<bool> ready (false);
+atomic_flag winner = ATOMIC_FLAG_INIT;
+
+void
+count_num(int id)
+{
+    while (!ready) {
+        this_thread::yield();
+    }
+
+    for (volatile int i = 0; i < 1000000; i++) {}
+    cout<<id;
+    if (!winner.test_and_set())
+        cout<<"Winner is "<<id<<endl;
+}
+
+void
+test_yield()
+{
+    cout<<"testing yield"<<endl;
     
+    vector<thread> vec;
+    for (int i = 0; i < 10; i++)
+        vec.push_back(thread(count_num, i));
+    
+    ready = true;
+    
+    for (auto & th : vec)
+        th.join();
+    
+    cout<<endl<<"testing yield finished"<<endl;
+}
+
+int
+main(int argc, char** argv)
+{
+    test_join();
     test_detach();
+    test_yield();
     cout<<"all theads are completed."<<endl;
 
 }
