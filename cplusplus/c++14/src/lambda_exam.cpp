68bc469bc0a97e4874188c24d141bbb53210678e
yu2003w <yu2003w@hotmail.com>
2017-03-02 18:43:08 +0800

    add source code for lambda

--- cplusplus/c++14/src/lambda_exam.cpp
+++ cplusplus/c++14/src/lambda_exam.cpp
@@ -1,3 +1,20 @@
-#include <string>
+/*
+ *
+ * Initiated by Jared on March 2, 2017.
+ *
+ */
+
 #include <iostream>
+#include "lambda_exam.hpp"
+
 using namespace std;
+
+void
+LambdaExam::test_basic()
+{
+    int a = 0;
+    auto f = [=]() { return a*2; };
+    auto val = f();
+    cout<<val<<endl;
+    return; 
+}
